4e314bcb6ebe0df12f2bc22abb76d6d5e741b7db
shannone <shannone426@gmail.com>
2022-08-05 13:45:41 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 
 // Write your code here
+
+ 
+
+
+
+
+
+/* 1) multiply is an equation whose multiplied results will equal 62:
+     ReferenceError: num1 is not defined
+     num1 * num2 = 62
+*/  
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+ /* 2) random
+         generates a random integer greater than 0:
+         Math.random()
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+         The Math.floor() function returns the largest integer less than or equal to a given number.)
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+ */    
+const random = Math.floor(Math.random() * 10) +1;
+
+/*  3) mod 
+         is an equation that calculates a remainder that is equal to 4:
+         num3 % num4 = 4 */
+let num3 = 25;
+let num4 = 7;
+const mod = num3 % num4;
+
+/*  4) max
+         will return 20 as the highest number in the set:
+         look up Math Method for this I forget
+         lmao duhh Shan its Math.max()
+          https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+*/      
+ const max = Math.max(1, 20);
